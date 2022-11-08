b0f56ebdb939c1f7825673a9aee4597a8a4854de
zahraad77 <108978586+zahraad77@users.noreply.github.com>
2022-11-08 19:15:43 +0330

    Add files via upload

---
+++ tmrin3.cs
@@ -0,0 +1,13 @@
+using system;
+class test
+{
+public static void main (string[]args)
+{
+    string input = console.readline();
+    int num = convert.ToInt32(input);
+    num +=10;
+    console.writeline(num);
+
+}
+
+}
